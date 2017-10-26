0fb61a9ead79220778e7e266f6ec9d7db3b1a92f
15001260922 <2639654737@qq.com>
2017-10-26 12:04:54 +0800

    dsadsa

--- app/src/main/java/com/example/dell/myapplication/TextTwoActivity.java
+++ app/src/main/java/com/example/dell/myapplication/TextTwoActivity.java
@@ -3,6 +3,7 @@ package com.example.dell.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by dell on 2017/10/26.
@@ -12,5 +13,6 @@ public class TextTwoActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("dsaf","dsaf");
     }
 }

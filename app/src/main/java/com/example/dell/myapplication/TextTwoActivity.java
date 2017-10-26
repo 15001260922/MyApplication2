8a524f02701eecceea3f1c433850bc97e639b9bc
15001260922 <2639654737@qq.com>
2017-10-26 11:51:23 +0800

    第二版本

---
+++ app/src/main/java/com/example/dell/myapplication/TextTwoActivity.java
@@ -0,0 +1,16 @@
+package com.example.dell.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by dell on 2017/10/26.
+ */
+
+public class TextTwoActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
